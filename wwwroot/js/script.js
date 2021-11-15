c7c60d86f7220f6a3de2c63b6a3cbfc4612a4574
vishal.garg@idera.com <vishal.garg@idera.com>
2021-11-15 17:35:45 +0530

    Added The Charts

---
+++ wwwroot/js/script.js
@@ -0,0 +1,57 @@
+﻿alert('Welcome to FusionCharts with Blazor APP');
+
+function GetStudentName() { 
+    //STEP 2 - Chart Data
+    const chartData = [{
+        "label": "Venezuela",
+        "value": "290"
+    }, {
+        "label": "Saudi",
+        "value": "260"
+    }, {
+        "label": "Canada",
+        "value": "180"
+    }, {
+        "label": "Iran",
+        "value": "140"
+    }, {
+        "label": "Russia",
+        "value": "115"
+    }, {
+        "label": "UAE",
+        "value": "100"
+    }, {
+        "label": "US",
+        "value": "30"
+    }, {
+        "label": "China",
+        "value": "30"
+    }];
+
+    //STEP 3 - Chart Configurations
+    const chartConfig = {
+        type: 'column2d',
+        renderAt: 'chart-container',
+        width: '100%',
+        height: '400',
+        dataFormat: 'json',
+        dataSource: {
+            // Chart Configuration
+            "chart": {
+                "caption": "Countries With Most Oil Reserves [2017-18]",
+                "subCaption": "In MMbbl = One Million barrels",
+                "xAxisName": "Country",
+                "yAxisName": "Reserves (MMbbl)",
+                "numberSuffix": "K",
+                "theme": "fusion",
+            },
+            // Chart Data
+            "data": chartData
+        }
+    };
+    FusionCharts.ready(function () {
+        var fusioncharts = new FusionCharts(chartConfig);
+        fusioncharts.render();
+    });
+
+}
